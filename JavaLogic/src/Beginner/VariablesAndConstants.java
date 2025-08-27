be2c4e6f975cbc9706db457e1c616c07b5fba633
WikyDev <wikinson72@gmail.com>
2025-08-27 16:09:02 -0500

    Primer estudio

---
+++ JavaLogic/src/Beginner/VariablesAndConstants.java
@@ -0,0 +1,23 @@
+package Beginner;
+
+public class VariablesAndConstants {
+    public static void main(String[] args) {
+        //Variables
+        String name = "Wiky";
+        System.out.println(name + " " + ";)");
+        name = "Wilson";
+
+        int age;
+        //Constants
+        age = 25;
+        System.out.println(age);
+
+        var apellido = "Ospina";
+        System.out.println(apellido);
+        //Constants
+
+        final String EMAIL = "wiky@gmail.com";
+        // email = "hola";
+        System.out.println(EMAIL);
+    }
+}
