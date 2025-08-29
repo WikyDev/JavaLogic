a29ae0c8d4efd702516f7ab008464942487509c1
WikyDev07 <wikinson722@gmail.com>
2025-08-28 20:53:43 -0500

    Act.1 de ejercicios

--- JavaLogic/src/Beginner/BeginnerExercises.java
+++ JavaLogic/src/Beginner/BeginnerExercises.java
@@ -10,18 +10,23 @@ public class BeginnerExercises {
     public static void main(String[] args) {
 
         // 1. Declara una variable de tipo String y asígnale tu nombre.
-
-
+        String name = "Wilson";
+        System.out.println(name);
         // 2. Crea una variable de tipo int y asígnale tu edad.
-
+        int edad = 25;
+        System.out.println(edad);
         // 3. Crea una variable double con tu altura en metros.
-
+        double altura = 1.82;
+        System.out.println(altura);
         // 4. Declara una variable de tipo boolean que indique si te gusta programar.
-
+        boolean indicador = true;
+        System.out.println(indicador);
         // 5. Declara una constante con tu email.
-
+        final String EMAIL = "Wiky@gmail.com";
+        System.out.println(EMAIL);
         // 6. Crea una variable de tipo char y guárdale tu inicial.
-
+        char letra = 'W';
+        System.out.println(letra);
         // 7. Declara una variable de tipo String con tu localidad, y a continuación cambia su valor y vuelve a imprimirla.
 
         // 8. Crea una variable int llamada a, otra b, e imprime la suma de ambas.
