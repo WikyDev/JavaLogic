386b05f8b479bde881e3165c2609578ad23b42b2
WikyDev <wikinson72@gmail.com>
2025-09-27 12:13:24 -0500

    Falta 1 ejercicio Loops

--- JavaLogic/src/Loops/ExercicesLoops.java
+++ JavaLogic/src/Loops/ExercicesLoops.java
@@ -7,6 +7,8 @@ Vídeo: https://youtu.be/JOAqpdM36wI?t=17993
 
 import java.sql.Array;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 
 public class ExercicesLoops {
 
@@ -47,15 +49,44 @@ public class ExercicesLoops {
         }
         System.out.println(suma);
         // 5. Usa un for para recorrer un Array y mostrar sus valores.
-
+        for(int w = 0; w < o.length; w++){
+            System.out.println(o[w]);
+        }
         // 6. Usa for-each para recorrer un HashSet y un HashMap.
+        HashSet<String> sett = new HashSet<>();
+        sett.add("Wilson");
+        sett.add("Andres");
+        sett.add("Ospina");
 
+        HashMap<Integer, String> mapa = new HashMap<>();
+        mapa.put(25, "Hinestroza");
+        mapa.put(30, "Anyi");
+
+        for (String setters : sett){
+            System.out.println(setters);
+        }
+        for (var mapas : mapa.entrySet()){
+            System.out.println("Edad: " + mapas.getKey() + "," + "Nombre: " +mapas.getValue() + "\n");
+        }
         // 7. Imprime los números del 10 al 1 (descendiente) con un bucle for.
-
+        for (int z = 10; z < 11 && z > 0; z--){
+            System.out.println(z);
+        }
         // 8. Usa continue para saltar los múltiplos de 3 del 1 al 20.
-
+        for(int q = 1; q <= 20; q++){
+            if (q % 3 == 0){
+                continue;
+            }
+            System.out.println(q);
+        }
         // 9. Usa break para detener un bucle cuando encuentres un número negativo en un array.
+        int[] negativos = new int[]{15,20,7,30,-8};
 
-        // 10. Crea un programa que calcule el factorial de un número dado.
+        for (int r : negativos) {
+            if (negativos.equals(-8)) {
+                System.out.println("Hola");
+                break;
+            }
+        }// 10. Crea un programa que calcule el factorial de un número dado.
     }
 }
